b652170fb26d78f1cb87f4f2e647a59a3ea5c32f
sabjorn <info@sabjorn.net>
2022-03-16 20:23:35 -0700

    got the send/receiving working, fetch not working fully yet

--- src/background/playlist_backend.js
+++ src/background/playlist_backend.js
@@ -4,30 +4,36 @@ export default class PlaylistBackend {
   constructor() {
     this.log = new Logger();
     this.fetchPlaylistData = PlaylistBackend.fetchPlaylistData.bind(this);
+    this.connectionListenerCallback = PlaylistBackend.connectionListenerCallback.bind(this);
   }
 
   init() {
     this.log.info("starting playlist backend.");
-    chrome.runtime.onMessage.addListener(
-      function(request, sender, sendResponse) {
-        console.log(sender.tab ?
-                    "from a content script:" + sender.tab.url :
-                    "from the extension");
-        if (request.greeting === "hello")
-          sendResponse({farewell: "goodbye"});
-      }
-    );
+    chrome.runtime.onConnect.addListener(this.connectionListenerCallback);
   }
 
   static fetchPlaylistData(request, sender, sendResponse) {
     //if (request.contentScriptQuery != "renderBuffer") return;
     this.log.info("url recieved, grabbing track data.");
-    //const url = request.url;
-    //fetch(url)
-    //  .then(response => { this.log.info(response.text()); })
-    //  //.then(rmsBuffer => sendResponse(rmsBuffer))
-    //  .catch(error => this.log.error(error));
+    const url = request.url;
+    fetch(url)
+      .then(response => { this.log.info(response.text()); })
+      //.then(rmsBuffer => sendResponse(rmsBuffer))
+      .catch(error => this.log.error(error));
 
-    //  return true;
+      return true;
+  }
+  
+  static connectionListenerCallback(port) {
+    this.log.info("connection listener callback");
+    if (port.name !== "bandcamplabelview") {
+      this.log.error(
+        `Unexpected chrome.runtime.onConnect port name: ${port.name}`
+      );
+      return;
+    }
+
+    this.port = port;
+    this.port.onMessage.addListener(this.fetchPlaylistData);
   }
 }
--- src/playlist.js
+++ src/playlist.js
@@ -3,10 +3,17 @@ import Logger from "./logger";
 export default class Playlist {
   constructor() {
     this.log = new Logger();
+    this.port = chrome.runtime.connect(null, { name: "bandcamplabelview" });
   }
 
   init() {
     this.log.info("Loaded Playlist");
+    this.button = document.querySelector("button");
+
+    this.button.addEventListener("click", () => {
+      this.log.info("hi");
+      this.port.postMessage({ url:"https://halfpastvibe.bandcamp.com/album/vielen-dank" });
+    });
   }
 }
 
