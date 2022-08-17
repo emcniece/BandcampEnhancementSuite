b7753bf36e024111fbada67b5d2f8b75afded2fe
Eric McNiece <emcniece@gmail.com>
2022-08-17 11:35:21 -0700

    Testing CI ENV variables

--- snapshots.js
+++ snapshots.js
@@ -7,7 +7,7 @@ const snapOptions = {
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: [
       '--disable-extensions-except=./',
       '--load-extension=./',
@@ -21,8 +21,9 @@ const snapOptions = {
   await page.goto('https://halfpastvibe.bandcamp.com/album/vielen-dank', { waitUntil: 'networkidle2' });
   await page.waitForSelector('#trackInfoInner .volume.thumb')
   await page.waitForTimeout(2000)
-  await percySnapshot(page, 'Example Site', snapOptions);
+  const output = await percySnapshot(page, 'Example Site', snapOptions);
 
+  console.log(output)
   // Comment browser.close() to keep the testing window open when running with `node snapshots.js`
   await browser.close();
 })();
